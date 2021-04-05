f1449f86d611d0040f6ac13f9ccc7e95b239f98f
linkshooter1 <stepan.yurikov.2005@gmail.com>
2021-04-05 15:22:28 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -4,8 +4,8 @@ key = 'key from market api'
 def pinging():
     while True:
         1==1 
-        urlsvin = 'https://market.csgo.com/api/v2/ping?key='+key
-        ping = requests.get(urlsvin).json()
+        url = 'https://market.csgo.com/api/v2/ping?key='+key
+        ping = requests.get(url).json()
         print(ping['success'])
         if ping['success'] == False:
             print(ping)
