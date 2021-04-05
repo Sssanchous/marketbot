86b57b89bae3d0531b4d3433d97f724e985e87f2
linkshooter1 <stepan.yurikov.2005@gmail.com>
2021-04-05 15:33:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,8 +94,8 @@ def price_f6():
     #urlsteam = "http://steamcommunity.com/market/priceoverview/?appid=570&market_hash_name="+name
     #steamp = requests.get(urlsteam)").json()
     #print(steamp)
-    def balance():
-    urlmoneysex = 'https://market.csgo.com/api/v2/get-money?key='+key
-    request = requests.get(urlmoneysex).json()
+def balance():
+    urlmoney = 'https://market.csgo.com/api/v2/get-money?key='+key
+    request = requests.get(urlmoney).json()
     print(request['success'])
     print(request['money'])
